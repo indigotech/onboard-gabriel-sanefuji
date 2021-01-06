58fe6ad7936cebee082ab95a56c0c94428f9919d
Sane98 <gsanefuji@uol.com.br>
2021-01-06 11:19:04 -0300

    changes suggested by Alan and runs on both IOS and Android

--- Poke/.eslintrc.js
+++ Poke/.eslintrc.js
@@ -10,4 +10,8 @@ module.exports = {
     'prettier/@typescript-eslint',
     'plugin:jest/recommended',
   ],
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+  },
 };
