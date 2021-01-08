45aaff94fa8d7b7d8b9028f2949186f9ba2aef5e
Sane98 <gsanefuji@uol.com.br>
2021-01-08 03:34:35 -0300

    validation with switch case

--- Poke/src/validation.ts
+++ Poke/src/validation.ts
@@ -1,27 +1,23 @@
 export function Validation(email: string, password: string) {
   var error;
-  if(email===''){ 
-    error = 'O campo de e-mail deve estar preenchido';
-    return error;
-  }
-  else if(!(email.match(/.+(@{1}.+.com{1}$)/))){
-    error = 'O e-mail foi escrito errado';
-    return error;
-  }
-  else if(password===''){
-    error = 'O campo de senha deve estar preenchido';
-    return error;
-  }
-  else if(password.length < 7){
-    error = 'A senha deve ter no mínimo 7 caracteres';
-    return error;
-  }
-  else if(!(password.match(/^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$/))){
-    error = 'A senha deve ter no mínimo um caracter e um número';
-    return error;
-  }
-  else{
-    error = 'Tudo certo';
-    return error;
+  switch(true){
+    case (email===''):
+      error = 'O campo de e-mail deve estar preenchido';
+      return error;
+    case (!(email.match(/.+(@{1}.+.com{1}$)/))):
+      error = 'O e-mail foi escrito errado';
+      return error;
+    case (password===''):
+      error = 'O campo de senha deve estar preenchido';
+      return error;
+    case (password.length < 7):
+      error = 'A senha deve ter no mínimo 7 caracteres';
+      return error;
+    case (!(password.match(/^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$/))):
+      error = 'A senha deve ter no mínimo um caracter e um número';
+      return error;
+    default:
+      error = 'Tudo certo';
+      return error;
   }
 }
