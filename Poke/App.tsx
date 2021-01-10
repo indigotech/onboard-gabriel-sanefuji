126dbf13126cc592591bc0a5133dcccb81753130
Sane98 <gsanefuji@uol.com.br>
2021-01-10 13:17:06 -0300

    connection to apollo server and store token on ios

--- Poke/App.tsx
+++ Poke/App.tsx
@@ -6,38 +6,64 @@ import {Colors} from 'react-native/Libraries/NewAppScreen';
 import {EmailInput, PasswordInput} from './src/components/login-input';
 import {SubmitButton} from './src/components/submit-button';
 import {validation} from './src/validation';
-
-import {ApolloClient, gql, InMemoryCache} from '@apollo/client';
+import {ApolloClient, InMemoryCache, gql} from '@apollo/client';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const client = new ApolloClient({
   uri: 'https://tq-template-server-sample.herokuapp.com/graphql',
   cache: new InMemoryCache()
 });
 
+// Email: admin@taqtile.com.br
+// Password: 1234qwer
+
+const login = (email: string, password: string) => {
+   client.mutate({
+    mutation: gql`
+      mutation {
+        login (data:{
+          email: "${email}"
+          password: "${password}"
+        }){
+          token
+        }
+      }
+    `
+  })
+  .then((result) => {
+    console.log(result)
+    const jsonString = JSON.stringify(result);
+    const data = JSON.parse(jsonString);
+    storeData(data.data.login.token);
+  })
+  .catch(err => {
+    const errorString = JSON.stringify(err)
+    const error = JSON.parse(errorString)
+    console.log(error)
+    Alert.alert(error.message)
+  })
+}
+
+const storeData = async (value: string) => {
+  try {
+    await AsyncStorage.setItem('@storage_Key', value)
+    Alert.alert("Nice")
+  } catch (e) {
+    Alert.alert(e)
+  }
+}
+
 const App = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  
   const handleSubmit = () => {
-    const erro = validation(email, password);
-    Alert.alert(erro);
-    if(erro === 'Tudo certo'){
-      client
-      .mutate({
-        mutation: gql`
-        mutation{
-          login(data:{
-            email: "${email}"
-            password: "${password}"
-          }){
-            token
-          }
-        }
-      `
-      })
-      .then(result => console.log(result));
+    const validationError = validation(email, password);
+    if(validationError === 'Tudo certo'){
+      login(email, password)
     }
-    
+    else{
+      Alert.alert(validationError);
+    }
   };
   return (
     <>
@@ -48,7 +74,7 @@ const App = () => {
             <Text style={styles.simple}>Bem vindo(a) à Taqtile!</Text>
             <EmailInput text={email} onTextChange={setEmail} />
             <PasswordInput text={password} onTextChange={setPassword} />
-            <SubmitButton onTap={() => {handleSubmit();}} />
+            <SubmitButton onTap={() => {handleSubmit()}} />
           </View>
         </ScrollView>
       </SafeAreaView>
