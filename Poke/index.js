855b91e93cf53b45f9a2c346399b8e752d60c3cd
Sane98 <gsanefuji@uol.com.br>
2021-01-11 09:39:13 -0300

    configurations for navigation

--- Poke/index.js
+++ Poke/index.js
@@ -1,6 +1,20 @@
-import {AppRegistry} from 'react-native';
+import { Navigation } from "react-native-navigation";
 import App from './App';
-import {name as appName} from './app.json';
 
+Navigation.registerComponent('com.myApp.WelcomeScreen', () => App);
 
-AppRegistry.registerComponent(appName, () => App);
+Navigation.events().registerAppLaunchedListener(() => {
+  Navigation.setRoot({
+     root: {
+       stack: {
+         children: [
+           {
+             component: {
+               name: 'com.myApp.WelcomeScreen'
+             }
+           }
+         ]
+       }
+     }
+  });
+});
